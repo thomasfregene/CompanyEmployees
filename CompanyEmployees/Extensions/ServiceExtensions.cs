9fcf6c9dc3a7b50c6bbcde79bcba8c0076beb8d7
thomasfregene <fregzythomas@gmail.com>
2024-09-05 08:52:44 +0100

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurCors(this IServiceCollection services) =>
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(opt =>
+            {
+            });
+
+    }
+}
