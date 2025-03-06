d4a4b3066f353db58b61ba2982d9bf1a0a1a1745
thomasfregene <fregzythomas@gmail.com>
2025-03-06 14:53:03 +0100

    sorting data

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -27,15 +27,17 @@ namespace Repository
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChange)
                 .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
                 .Search(employeeParameters.SearchTerm)
-            .OrderBy(e => e.Name)
+                .Sort(employeeParameters.OrderBy)
+            //.OrderBy(e => e.Name)
             .ToListAsync();
+
             /*var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChange)
              .OrderBy(e => e.Name)
              .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
              .Take(employeeParameters.PageSize)
              .ToListAsync();*/
 
-            var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChange).CountAsync();
+            //var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChange).CountAsync();
 
             return PageList<Employee>.ToPageList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
             //return new PageList<Employee>(employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using Entities.Models;
-
+using System.Linq.Dynamic.Core;
+using Repository.Extensions.Utility;
 namespace Repository.Extensions
 {
     public static class RepositoryEmployeeExtensions
@@ -21,6 +19,20 @@ namespace Repository.Extensions
 
             return employees.Where(e=>e.Name.ToLower().Contains(lowerCaseTerm));
         }
-        
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            if(string.IsNullOrWhiteSpace(orderByQueryString))
+                return employees.OrderBy(e=>e.Name);
+
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+
+            return employees.OrderBy(orderQuery);
+
+        }
+
     }
 }
